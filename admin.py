095dfbf7999a61ff21396833388d1aa9e15ebee3
jiayang <jchen59@stuy.edu>
2019-01-05 17:57:37 -0500

    fixed indentation of load command

--- admin.py
+++ admin.py
@@ -23,22 +23,22 @@ class Admin:
             print('SUCCESSFULLY RELOADED: ' + module)
             await ctx.send(('SUCCESSFULLY RELOADED: ' + module))
 
-            @commands.command(name='load',hidden=True)
-            async def _load(self, ctx, module : str, pull = ''):
-                """Loads a module."""
-                if pull == '-p':
-                    g = git.cmd.Git('.')
-                    g.pull()
-                    print('Pulled from git')
-                    await ctx.send('Successfully pulled from git')
-                try:
-                    bot.load_extension(module)
-                except Exception as e:
-                    print('{}: {}'.format(type(e).__name__, e))
-                    await ctx.send('{}: {}'.format(type(e).__name__, e))
-                else:
-                    print('SUCCESSFULLY LOADED: ' + module)
-                    await ctx.send('SUCCESSFULLY LOADED: ' + module)
+    @commands.command(name='load',hidden=True)
+    async def _load(self, ctx, module : str, pull = ''):
+        """Loads a module."""
+        if pull == '-p':
+            g = git.cmd.Git('.')
+            g.pull()
+            print('Pulled from git')
+            await ctx.send('Successfully pulled from git')
+        try:
+            bot.load_extension(module)
+        except Exception as e:
+            print('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('{}: {}'.format(type(e).__name__, e))
+        else:
+            print('SUCCESSFULLY LOADED: ' + module)
+            await ctx.send('SUCCESSFULLY LOADED: ' + module)
 
 def setup(bot):
     bot.add_cog(Admin(bot))
