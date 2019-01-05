b181704e14b233079c3b53833148ab5feca5d817
jiayang <jchen59@stuy.edu>
2019-01-05 18:11:15 -0500

    woops please

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.voice_client import VoiceClient
 import discord
 
 class Music:
@@ -10,7 +11,7 @@ class Music:
     @commands.command(name='join',)
     async def _join(self,ctx):
         '''Join the voice channel'''
-        await self.bot.join_voice_channel(ctx.author.voice_channel)
+        await self.bot.join_voice_channel(ctx.message.author.voice_channel)
         await ctx.message.delete()
 
 
