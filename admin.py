f1d9de90d7e00fe0c2786d4f22bde92cbb804da8
Johnson Li <jli39@stuy.edu>
2019-01-05 22:28:58 -0500

    fixed msg

--- admin.py
+++ admin.py
@@ -19,7 +19,7 @@ class Admin:
             print('Pulled from git')
             repo = git.Repo(os.getcwd())
             master = repo.head.reference
-            await ctx.send(f'{master.commit.author.name}: {master.commit.message}')
+            await ctx.send(f'**Last Commit**: {master.commit.author.name}- {master.commit.message}')
             await ctx.send('Successfully pulled from git')
         try:
             self.bot.unload_extension(module)
