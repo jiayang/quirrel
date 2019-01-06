914d431a11682a215ec7cf326b3dff1a3edb9134
jiayang <jchen59@stuy.edu>
2019-01-05 21:39:19 -0500

    try4

--- music.py
+++ music.py
@@ -4,7 +4,21 @@ from discord.ext.commands import Bot
 from discord.voice_client import VoiceClient
 import discord
 
-opus.load_opus('bin/libopus-0.x64.dll')
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+def load_opus_lib():
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            opus.load_opus(opus_lib)
+            return
+        except OSError:
+            pass
+
+
+if not discord.opus.is_loaded():
+discord.opus.load_opus()
 
 class Music:
 
