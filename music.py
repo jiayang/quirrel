e9c4bb8c4612a529da36e92c9533ed9beedcd1d4
jiayang <jchen59@stuy.edu>
2019-02-12 22:06:23 -0500

    afk leaves if noone is in the call after the end

--- music.py
+++ music.py
@@ -189,6 +189,9 @@ class Playlist:
 
     #Wait a few ms before playing the next song - prevents abrupt transitions
     def after(self,e):
+        if len(self.vc.channel.members) == 1:
+            await self.vc.disconnect()
+            return
         asyncio.run_coroutine_threadsafe(asyncio.sleep(20), self.loop)
         asyncio.run_coroutine_threadsafe(self.play(), self.loop)
 
