b1a933593314fbe4ee0f56369708f22cfc3fe696
jchen59 <jchen59@stuy.edu>
2021-04-11 21:48:41 +0000

    begin barebones image system, using img api hosted on jiayang.dev

---
+++ cogs/image.py
@@ -0,0 +1,45 @@
+from util import dio
+
+from discord.ext import commands
+import discord
+import asyncio
+import requests
+
+class Image(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='image', aliases=['i'])
+    async def _image(self,ctx, *args):
+        '''Displays the image from a user-specific library saved'''
+        if (len(args) == 0):
+            await ctx.send('Error, you need to give a keyword that you added with **!a [keyword]**')
+        key = " ".join(args)
+        payload = {
+            'uid': ctx.author.id,
+            'key': key
+        }
+        r = requests.get('https://img.jiayang.dev/search/', params=payload)
+        await ctx.send(r.text)
+        await ctx.message.delete()
+
+    @commands.command(name='add', aliases=['a'])
+    async def _add_image(self,ctx, *args):
+        '''Add an image into a user's library'''
+        if len(args) == 0:
+            await ctx.send('Error, to properly use type !add [keyword], then you will be prompted to enter an image')
+            return
+        key = " ".join(args)
+        garbage = []
+        response = await dio.prompt(ctx, "**Enter the image**", garbage)
+        await ctx.channel.delete_messages(garbage)
+        payload = {'uid': ctx.author.id,
+                   'key': key,
+                   'link': response.content
+                   }
+        requests.post('https://img.jiayang.dev/add/', params=payload)
+        await ctx.message.delete()
+        
+def setup(bot):
+    bot.add_cog(Image(bot))
