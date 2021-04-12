f4c1f55373678bc4b5f562603dfecb80d7675713
jchen59 <jchen59@stuy.edu>
2021-04-12 19:08:59 +0000

    added shortcut for emote

--- cogs/image.py
+++ cogs/image.py
@@ -10,23 +10,19 @@ class Image(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='image', aliases=['i'])
-    async def _image(self,ctx, *args):
-        '''Displays the image from a user-specific library saved'''
-        if (len(args) == 0):
-            await ctx.send('Error, you need to give a keyword that you added with **!a [keyword]**')
-        key = " ".join(args)
+    def fetch(key):
         payload = {
             'uid': 1024,
             'key': key
         }
         r = requests.get('https://img.jiayang.dev/search/', params=payload)
-        if r.status_code == 400:
-            await ctx.send('Error, something went wrong. Try again later')
-        else:
-            await ctx.send(r.text)
+        return r
+        
+    @commands.command(name='image', aliases=['i'])
+    async def _image(self,ctx, *args):
+        '''Displays the image from a user-specific library saved'''
+        await ctx.send('Heads up! I moved the images so you can now do `;[emote]`')
         await ctx.message.delete()
-
     @commands.command(name='add', aliases=['a'])
     async def _add_image(self,ctx, *args):
         '''Add an image into a user's library'''
@@ -51,9 +47,39 @@ class Image(commands.Cog):
         r = requests.post('https://img.jiayang.dev/add/', params=payload)
         if r.status_code == 400:
             await ctx.send('Error, to properly use type !add [keyword]')
-        if r.status_code == 401:
+        elif r.status_code == 401:
             await ctx.send('That keyword already exists. Use another')
+        else:
+            await ctx.send('Successfully added. A list of all emotes can be found using !emotes')
         await ctx.message.delete()
-        
+
+    @commands.command(name='emotes')
+    async def _list_links(self, ctx):
+        r = requests.get('https://img.jiayang.dev/search/', params={'uid' : 1024})
+        if r.status_code != 200:
+            await ctx.send('Error: something went wrong')
+        else:
+            await ctx.send(r.text)
+
+    @commands.command(name='delete')
+    async def _delete_emote(self, ctx, *args):
+        key = ' '.join(args)
+        r = requests.get('https://img.jiayang.dev/delete/', params={'uid': 1024, 'key': key})
+        if r.status_code == 403:
+            await ctx.send('Error: could not find an emote with key: ' + key)
+        else:
+            await ctx.send('Sucessfully deleted: ' + key)
+        await ctx.message.delete()
+    
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        '''If the message begins with ; , try to use the emote/whatever is associated'''
+        if msg.content[0] == ';':
+            key = msg.content[1:]
+            r = Image.fetch(key)
+            if r.status_code == 200:
+                await msg.delete()
+                await msg.channel.send(r.text)
+
 def setup(bot):
     bot.add_cog(Image(bot))
